132b0850d979f45efd17d54418c28fa5dca8db55
Marcos Victor <marcos@conveniar.com.br>
2021-10-19 22:44:35 -0300

    alterado o controller

--- controllers/products.js
+++ controllers/products.js
@@ -11,12 +11,17 @@ const getAllProductsStatic = async(req, res) => {
 
 const getAllProducts = async(req, res) => {
    const { featured, company, name, sort, fields } = req.query
-   const queryObject = query(featured, company, name)
-   
+   const queryObject = query(featured, company, name)   
    let result = Product.find(queryObject)
+   
    result = ordenar(result, sort)
    result = selectFields(result, fields)   
 
+   const page = Number(req.query.page) || 1
+   const limit = Number(req.query.limit) || 10
+   const skip = (page -1) * limit;
+
+   result = result.skip(skip).limit(limit)
    const products = await result
 
    res.status(200).json({ nbHits: products.length, products })
